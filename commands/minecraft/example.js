9de3518de6b6379d2e3073fafe085e1c115d79ab
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2025-10-05 09:30:16 +0800

    fix: add errorHandler to example.js

--- commands/minecraft/example.js
+++ commands/minecraft/example.js
@@ -1,10 +1,12 @@
+const { withErrorHandling } = require("../../utils/commandHandler");
+
 module.exports = {
     name: 'example',
     aliases: [],
     description: '這是一個範例指令',
     usage: '<args>',
     requiredPermissionLevel: 0, // default permission level
-    execute,   
+    execute: withErrorHandling(execute),
 }
 
 async function execute(bot, playerId, args) {
