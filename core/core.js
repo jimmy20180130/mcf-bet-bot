5d887bce6228511641c06e6c5bb91239aa56b446
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2025-10-07 23:03:46 +0800

    feat: add service manager

--- core/core.js
+++ core/core.js
@@ -1,6 +1,7 @@
 const Logger = require("../utils/logger");
 const fs = require('fs');
-const client = require('./client');
+const { mcClient } = require('./client');
+const serviceManager = require('../services/serviceManager');
 const mcBot = require('./mcBot')
 const rl = require('readline').createInterface({
     input: process.stdin,
@@ -9,23 +10,50 @@ const rl = require('readline').createInterface({
 
 const bot = new mcBot({
     username: 'saraun',
-    host: 'proxy-net-6.mcfallout.net',
+    host: 'proxy-net-5.mcfallout.net',
     port: 25565,
     auth: 'microsoft',
     version: '1.20.1'
 })
 
-// require all services to init them
-fs.readdirSync('./services').forEach(file => {
-    if (file.endsWith('.js')) {
-        require(`../services/${file}`);
-    }
-});
+// 註冊所有服務
+function registerServices() {
+    // Minecraft 專屬服務
+    serviceManager.register({ name: 'paymentService', path: '../services/minecraft/paymentService' });
+    serviceManager.register({ name: 'betService', path: '../services/minecraft/betService' });
+    serviceManager.register({ name: 'teleportService', path: '../services/minecraft/teleportService' });
 
-bot.start()
+    // General 服務
+    serviceManager.register({ name: 'authService', path: '../services/general/authService' });
+    serviceManager.register({ name: 'blacklistService', path: '../services/general/blacklistService' });
+    serviceManager.register({ name: 'databaseService', path: '../services/general/databaseService' });
+    serviceManager.register({ name: 'errorHandler', path: '../services/general/errorHandler' });
+    serviceManager.register({ name: 'errorHistoryService', path: '../services/general/errorHistoryService' });
+    serviceManager.register({ name: 'linkService', path: '../services/general/linkService' });
+    serviceManager.register({ name: 'rankService', path: '../services/general/rankService' });
+    serviceManager.register({ name: 'ticketService', path: '../services/general/ticketService' });
+    serviceManager.register({ name: 'userInfoService', path: '../services/general/userInfoService' });
+
+    // Commands 處理器
+    serviceManager.register({ name: 'commandHandler', path: '../commands/index' });
+}
+
+// 初始化所有服務
+async function initializeServices() {
+    registerServices();
+    await serviceManager.initialize();
+}
+
+// 啟動應用
+async function start() {
+    await initializeServices();
+    bot.start();
+}
+
+start();
 
 rl.on('line', (input) => {
-    if (client.mcBotInstance) client.mcBotInstance.chat(input);
+    if (mcClient.botInstance) mcClient.botInstance.chat(input);
 });
 
 process.on('uncaughtException', (err) => {
