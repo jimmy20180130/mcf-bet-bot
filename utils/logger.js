4893777760fe3d49ff08daa5c69a9fbb37c433d7
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2026-03-04 01:58:13 +0800

    fix: edit logger

--- utils/logger.js
+++ utils/logger.js
@@ -32,24 +32,25 @@ class Logger {
         this.log(message);
     }
 
+    _getFileInfo(stack) {
+        const match = stack.split('\n')[2].match(/\((.+?):(\d+):\d+\)$/);
+        if (match) {
+            const [, filePath, lineNumber] = match;
+            return `\x1b[2m(${filePath.split(/[/\\]/).pop()}:${lineNumber})\x1b[0m`;
+        }
+        return '';
+    }
+
     warn(message) {
         const timestamp = this._formatTimestamp(new Date());
         this.logs.push({ message, timestamp });
-
-        const fileInfo = ((stack) => {
-            const match = stack.split('\n')[2].match(/\((.+?):(\d+):\d+\)$/);
-            if (match) {
-                const [, filePath, lineNumber] = match;
-                return `\x1b[2m(${filePath.split(/[/\\]/).pop() + ':' + lineNumber})\x1b[0m`;
-            }
-            return '';
-        })(new Error().stack || '');
-        
+        const fileInfo = this._getFileInfo(new Error().stack || '');
         console.log(`[${timestamp}] \x1b[33m[WARN]\x1b[0m \x1b[43m${message}\x1b[0m ${fileInfo}`);
     }
 
-    error(...args) {  // 支援多個參數
+    error(...args) {
         const timestamp = this._formatTimestamp(new Date());
+        const stack = new Error().stack || '';
         const message = args.map(arg => {
             if (arg instanceof Error) {
                 return `${arg.message}\n${arg.stack}`;
@@ -59,18 +60,9 @@ class Logger {
                 return arg;
             }
         }).join(' ');
-    
+
         this.logs.push({ message, timestamp });
-    
-        const fileInfo = ((stack) => {
-            const match = stack.split('\n')[2].match(/\((.+?):(\d+):\d+\)$/);
-            if (match) {
-                const [, filePath, lineNumber] = match;
-                return `\x1b[2m(${filePath.split(/[/\\]/).pop() + ':' + lineNumber})\x1b[0m`;
-            }
-            return '';
-        })(new Error().stack || '');
-    
+        const fileInfo = this._getFileInfo(stack);
         console.log(`[${timestamp}] \x1b[31m[ERROR]\x1b[0m \x1b[41m${message}\x1b[0m ${fileInfo}`);
     }
 
@@ -78,19 +70,11 @@ class Logger {
         const timestamp = this._formatTimestamp(new Date());
         this.logs.push({ message, timestamp });
 
-        const fileInfo = ((stack) => {
-            const match = stack.split('\n')[2].match(/\((.+?):(\d+):\d+\)$/);
-            if (match) {
-                const [, filePath, lineNumber] = match;
-                return `\x1b[2m(${filePath.split(/[/\\]/).pop() + ':' + lineNumber})\x1b[0m`;
-            }
-            return '';
-        })(new Error().stack || '');
-
         if (!this.debugMode) return;
 
+        const fileInfo = this._getFileInfo(new Error().stack || '');
         console.log(`[${timestamp}] \x1b[34m[DEBUG]\x1b[0m ${message} ${fileInfo}`);
     }
 }
 
-module.exports = new Logger(true);
+module.exports = new Logger(false);
