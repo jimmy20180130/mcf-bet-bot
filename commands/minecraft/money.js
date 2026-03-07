7040f3835cf9af134b6cccfc5a35c8f5acb53365
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2026-03-07 21:41:49 +0800

    feat: add command money

--- commands/minecraft/money.js
+++ commands/minecraft/money.js
@@ -1,21 +1,5 @@
-// commands/minecraft/money.js
-
-const { addCommas } = require('../../utils/format');
-const { withErrorHandling } = require('../../utils/commandHandler');
-const Logger = require('../../utils/logger');
-
-module.exports = {
-    name: 'money',
-    aliases: [],
-    description: '查看目前 Bot 的餘額',
-    usage: '/m bot money',
-    requiredPermissionLevel: 1, // admin
-    execute: withErrorHandling('money', execute),   
-}
-
-async function execute(bot, playerId, args) {
-    Logger.log(`[money] ${playerId} 查詢 BOT 的餘額`);
-
+// money
+async function execute(bot, command, sender, args) {
     const map = bot.scoreboards?.["TAB-Scoreboard"]?.itemsMap;
 
     let emeraldRaw = map?.["§2§r"]?.displayName?.text?.match(/＄.*?([\d,]+)元/)?.[1];
@@ -24,5 +8,13 @@ async function execute(bot, playerId, args) {
     let villagerRaw = map?.["§3§r"]?.displayName?.text?.match(/§f([\d,]+)個/)?.[1];
     let villager = villagerRaw ? addCommas(parseInt(villagerRaw.replace(/,/g, ""))) : "無法取得";
 
-    bot.chat(`/m ${playerId} &a&l綠寶石&r&7: &b${emerald} &f個，&6&l村民錠&r&7: &b${villager} &f個`);
+    bot.chat(`/m ${sender} &a&l綠寶石&r&7: &b${emerald} &f個，&6&l村民錠&r&7: &b${villager} &f個`);
+    bot.logger.debug(`${sender} query money: ${emerald} emeralds, ${villager} coins`);
+}
+
+module.exports = {
+    name: 'money',
+    description: '查看目前 Bot 的餘額',
+    aliases: [],
+    execute
 }
