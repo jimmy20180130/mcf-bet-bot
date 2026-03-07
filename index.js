791edafb770187e1e59c8a9eb656ec76e5f2d14a
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2026-03-07 12:05:47 +0800

    feat: add auto reconnect

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const fs = require('fs');
 
 const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
 
+const mcBots = [];
+
 for (let i=0; i < config.bots.length; i++) {
     const mc = new mcBot(config.bots[i], i);
     mc.start();
+    mcBots.push(mc);
 }
+
+// if mcbot triggered end event, restart it after 5 seconds
+setInterval(() => {
+    mcBots.forEach((mc, index) => {
+        if (!mc.bot) {
+            console.warn(`[${mc.options.username}] 連線已斷開，正在嘗試重新連線...`);
+            mc.start();
+        }
+    });
+}, 5000);
