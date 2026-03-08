cde8740ca43221609f871031d9bb82b14c159588
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2026-03-08 12:43:48 +0800

    feat: add command link

---
+++ commands/minecraft/link.js
@@ -0,0 +1,11 @@
+// link
+async function execute(bot, command, sender, args) {
+    
+}
+
+module.exports = {
+    name: 'link',
+    description: '綁定 Discord 帳號',
+    aliases: ['綁定'],
+    execute
+}
