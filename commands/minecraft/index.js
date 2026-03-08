ae263ed1404c0f20ab635db9112b1cc2c83dc0da
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2026-03-08 11:48:45 +0800

    feat: add user check before executing the command

--- commands/minecraft/index.js
+++ commands/minecraft/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const User = require('../../models/User');
 
 const commandFiles = fs.readdirSync('./commands/minecraft').filter(file => file.endsWith('.js'));
 const commands = new Map();
@@ -16,6 +17,23 @@ for (const file of commandFiles) {
 async function executeCommand(bot, sender, command, args) {
     const cmd = commands.get(command);
     if (!cmd) return
+
+    let user = User.getByPlayerId(sender);
+    if (!user) {
+        const playeruuid = await bot.MinecraftDataService.getPlayerId(sender);
+        if (!playeruuid) {
+            return;
+        } else {
+            User.create({ playerid: sender, playeruuid });
+        }
+    }
+
+    user = User.getByPlayerId(sender);
+    if (!user) {
+        bot.chat(`/m ${sender} 執行指令時發生不可預期的錯誤，請稍後再試或聯繫機器人開發者`);
+        this.bot.logger.error(`無法找到且無法創建使用者資料: ${sender}`);
+        return;
+    }
 
     try {
         await cmd.execute(bot, command, sender, args);
